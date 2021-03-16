cafce2866430a6260ab545cdf66446d464891d18
nastya <nastyavolkm@tut.by>
2021-03-16 16:33:25 +0300

    init: start task cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
+module.exports = function countCats(matrix) {
   throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  
+  if (matrix === undefined){
+    return undefined;
+  }else if(matrix.length == 0){
+        return 0;
+    } else{
+    matrix = matrix.reduce(function(a, b){
+    return a.concat(b);
+});
+let count = 0;
+for(let i =0; i< matrix.length; i++){
+    if(matrix[i] == "^^"){
+        count++;
+    }
+}return count;
+}
 };
+
+
