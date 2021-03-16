d883d5057ae165cd056964cfa05e60d689033167
nastya <nastyavolkm@tut.by>
2021-03-17 02:43:41 +0300

    fix

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,11 +1,8 @@
-const CustomError = require("../extensions/custom-error");
-
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  throw new CustomError('Not implemented');
-  if(sampleActivity === undefined || typeof(sampleActivity) != "string" || isNaN(sampleActivity) === true){
+   if(sampleActivity === undefined || typeof(sampleActivity) != "string" || isNaN(sampleActivity) === true){
     return false;
   }else {
     return Math.ceil((Math.log(MODERN_ACTIVITY/Number(sampleActivity)))/(0.693/HALF_LIFE_PERIOD));
