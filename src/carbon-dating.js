1a688604ce79b47a6143aacf4bf896f37c967a4e
nastya <nastyavolkm@tut.by>
2021-03-21 22:59:44 +0300

    feat: finish task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,7 +2,7 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-   if(sampleActivity === undefined || typeof(sampleActivity) != "string" || isNaN(sampleActivity) === true){
+   if(sampleActivity === undefined || typeof(sampleActivity) !== "string" || isNaN(sampleActivity) || sampleActivity <= 0 || sampleActivity > 15){
     return false;
   }else {
     return Math.ceil((Math.log(MODERN_ACTIVITY/Number(sampleActivity)))/(0.693/HALF_LIFE_PERIOD));
