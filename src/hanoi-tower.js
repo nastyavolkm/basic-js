1dea66ac8b0b76ae5f00affd5b161e46f544c85c
nastya <nastyavolkm@tut.by>
2021-03-22 01:51:22 +0300

    init: start and finish task

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,9 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+    let hanoiObject = new Object();
+      hanoiObject.turns = Math.pow(2, disksNumber) - 1;
+      hanoiObject.seconds= Math.floor(hanoiObject.turns*3600/turnsSpeed);
+      return hanoiObject;
+    };
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+
+
