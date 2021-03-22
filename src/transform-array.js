7a562c7479d7e727c05ff444e3d160f92d2ad8bc
nastya <nastyavolkm@tut.by>
2021-03-22 16:08:59 +0300

    init: start task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,78 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(array) {
+  if(Array.isArray(array) === false) {
+    throw new CustomError('Not implemented');
+  }
+  let transformedArray = array.slice();
+  
+for(let i =0; i< transformedArray.length; i++){
+  if(transformedArray[i] == "--discard-next" && i==transformedArray.length-1){
+    transformedArray.splice(i, 1);
+  } else if(transformedArray[i] == "--discard-next" && i!==transformedArray.length-1){
+    transformedArray.splice(i, 2);
+    i=-1;
+  } else if (transformedArray[i] == "--discard-prev" && i ==0){
+    transformedArray.splice(i, 1);
+    i=-1;
+  }
+  else if (transformedArray[i] == "--discard-prev" && i>0) {
+    transformedArray.splice(i-1, 2);
+    i=-1;
+  } else if (transformedArray[i] == "--double-next" && i ==transformedArray.length-1){
+    transformedArray.splice(i, 1);
+  }else if (transformedArray[i] == "--double-next" && i !==transformedArray.length-1) {
+    transformedArray.splice(i+1, 0, transformedArray[i+1]);
+    transformedArray.splice(i, 1);
+    i=-1;
+  } else if (transformedArray[i] == "--double-prev" && i ==0) {
+    transformedArray.splice(i, 1);
+    i=-1;
+  } else if (transformedArray[i] == "--double-prev" && i >0){
+    transformedArray.splice(i-1, 0, transformedArray[i-1]);
+    transformedArray.splice(i, 1);
+    i=-1;
+  }
+}
+  return transformedArray;
+  
 };
+
+
+// function transform(array) {
+//   if(Array.isArray(array) === false) {
+//     throw new CustomError('Not implemented');
+//   }
+//   let transformedArray = array.slice();
+  
+// for(let i =0; i< transformedArray.length; i++){
+//   if(transformedArray[i] == "--discard-next" && i==transformedArray.length-1){
+//     transformedArray.splice(i, 1);
+//   } else if(transformedArray[i] == "--discard-next" && i!==transformedArray.length-1){
+//     transformedArray.splice(i, 2);
+//     i=-1;
+//   } else if (transformedArray[i] == "--discard-prev" && i ==0){
+//     transformedArray.splice(i, 1);
+//     i=-1;
+//   }
+//   else if (transformedArray[i] == "--discard-prev" && i>0) {
+//     transformedArray.splice(i-1, 2);
+//     i=-1;
+//   } else if (transformedArray[i] == "--double-next" && i ==transformedArray.length-1){
+//     transformedArray.splice(i, 1);
+//   }else if (transformedArray[i] == "--double-next" && i !==transformedArray.length-1) {
+//     transformedArray.splice(i+1, 0, transformedArray[i+1]);
+//     transformedArray.splice(i, 1);
+//     i=-1;
+//   } else if (transformedArray[i] == "--double-prev" && i ==0) {
+//     transformedArray.splice(i, 1);
+//     i=-1;
+//   } else if (transformedArray[i] == "--double-prev" && i >0){
+//     transformedArray.splice(i-1, 0, transformedArray[i-1]);
+//     transformedArray.splice(i, 1);
+//     i=-1;
+//   }
+// }
+//   console.log(transformedArray)
+// }
+
+
+// transform([4, 5, '--discard-next']);
