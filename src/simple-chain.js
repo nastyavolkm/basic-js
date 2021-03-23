3625b40e1f7642d79c220321da4152b823d044c8
nastya <nastyavolkm@tut.by>
2021-03-24 01:52:45 +0300

    init: start and finish simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,41 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  result: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.result.length;
+    
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    
+    if(value === undefined){
+      this.result.push(" ");
+    }else if(value === null) {
+      this.result.push("null");
+    } else {
+      this.result.push(value);
+    }
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+     if (typeof position !== "number" || (position ^ 0) !== position || position > this.getLength() || position <= 0) {
+      this.result = [];
+      throw new Error();
+    } else {
+      this.result.splice(position-1,1);
+    }
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.result.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let result = this.result;
+    this.result = [];
+    return `( ${result.join(' )~~( ')} )`;
+        
   }
 };
-
 module.exports = chainMaker;
+
