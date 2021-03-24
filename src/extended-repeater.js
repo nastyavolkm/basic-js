30263c80a1d382f734c5f5110c5d610bd7ec7886
nastya <nastyavolkm@tut.by>
2021-03-24 05:14:31 +0300

    init: finish task extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,53 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater(str, options){
+  if(typeof(str) === "object") {
+    str = ""+ str;
+  }
+  if(typeof(options.addition) === "object") {
+    options.addition = ""+ options.addition;
+  }
+  if(str === null){
+    str = "null";
+  }
+  if(options.addition === null){
+    options.addition = "null";
+  }
+  const objWithSpecificCoercion = {
+    [Symbol.toPrimitive]: hint => hint !== 'number' ? 'STRING_OR_DEFAULT' : 'NUMBER'
+  }
+  if(options.separator === undefined){
+    options.separator = "+";
+  }  
+  if(options.additionSeparator === undefined){
+    options.additionSeparator = "|";
+  }
+  if(options.addition === undefined){
+    options.addition = "";
+  }
+  if(options.addition !== undefined && options.additionRepeatTimes === undefined){
+    options.additionRepeatTimes = 1;
+  }
   
+  let repetearArray = [];
+      let repetearArray2 = [];
+      
+      if(options.additionRepeatTimes !== undefined && options.additionRepeatTimes >0){
+      
+      for(let i =1; i<= options.additionRepeatTimes; i++){
+      repetearArray.push(options.addition.toString());
+      }
+      repetearArray[0]=str.toString()+repetearArray[0];
+     
+      str = repetearArray.join(options.additionSeparator);
+      
+      }
+  if(options.repeatTimes !== undefined && options.repeatTimes >0){ 
+  
+  for(let i =1; i<=options.repeatTimes; i++){
+  repetearArray2.push(str);
+  }
+  
+  str = repetearArray2.join(options.separator);
+  
+  }
+  return str;
+  }
